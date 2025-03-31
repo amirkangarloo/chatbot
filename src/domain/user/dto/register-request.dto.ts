26adb66e4fb5a6713de9a245bbc14c135b7d8b21
Amir Kangarloo <amirkangarloo1051@gmail.com>
2025-03-31 19:58:52 +0330

    feat: Add swagger document for register api

--- src/domain/user/dto/register-request.dto.ts
+++ src/domain/user/dto/register-request.dto.ts
@@ -1,10 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class RegisterRequestDto {
+    @ApiProperty({
+        name: "username",
+        description: "unique username",
+        example: "john_doe",
+        type: String
+    })
     @IsString()
     @IsNotEmpty()
     username: string;
 
+    @ApiProperty({
+        name: "password",
+        description: "password",
+        example: "1234",
+        type: String
+    })
     @IsString()
     @IsNotEmpty()
     password: string;
